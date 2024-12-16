ea93ac752ccfc91b03d0a3e72457b1fcb78d1f5f
Inhishonor <inhishonor@protonmail.com>
2024-12-16 12:24:36 -0800

    Update fortune-teller.py

--- fortune-teller.py
+++ fortune-teller.py
@@ -1,16 +1,17 @@
 #Call's fortune's
 import fortune
-#Ask what the name and age is.
-name = input("What is your first name? ")
-print("Welcome to the Fortune Teller of Doom!!!")
-age = input("What is your age? ")
-#Print Name
-print("Hello "+ name + ".")
-x = len(name)
-#Determine Message
-if x < 4 or x == 4:
-    print(ds)
-elif x > 4 and x < 7:
+def main():
+    #Ask what the name and age is.
+    name = input("What is your first name? ")
+    print("Welcome to the Fortune Teller of Doom!!!")
+    age = input("What is your age? ")
+    #Print Name
+    print("Hello "+ name + ".")
+    x = len(name)
+    #Determine Message
+    if x < 4 or x == 4:
+        print(ds)
+    elif x > 4 and x < 7:
         y = float(age)
         if y < 10:
             print(ud)
@@ -22,10 +23,13 @@ elif x > 4 and x < 7:
             print(ni)
         else:
             print(ys)
-else:
-    nn = input("Pick a new number between 1 and 100. ")
-    z = float(nn)
-    if z < 50:
-        print("You will walk into the mouth of a Volcano while tap-dancing!")
     else:
-        print("While you are brushing your teeth you will be carried away by faries!")
+        nn = input("Pick a new number between 1 and 100. ")
+        z = float(nn)
+        if z < 50:
+            print("You will walk into the mouth of a Volcano while tap-dancing!")
+        else:
+            print("While you are brushing your teeth you will be carried away by faries!")
+
+if __name__ == '__main__':
+	main()

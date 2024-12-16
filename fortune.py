3cd477a234e3e66df146f739e87cd86a260142e8
Inhishonor <inhishonor@protonmail.com>
2024-12-16 11:27:54 -0800

    Create fortune.py

---
+++ fortune.py
@@ -0,0 +1,10 @@
+#Define all Variables/Messages
+ds = "You shall be defenestrated while sitting in a skyscraper!"
+rc = "You will be in a raccon race and suddenly get eaten by a cobra!"
+ud = "You shall see a unicorn and disapear forever!"
+ss = "You will be in the Sahara and get run over by Santa Claus!"
+sc = "You will be walking on a street and suddenly will combust!"
+ni = "You will win Miss. New Mexico and marry an iguana!"
+ys = "You will surive only to die a horrible death! :)"
+
+

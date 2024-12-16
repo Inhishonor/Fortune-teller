9f5bb79a95d36769016f0bc0b313e973026d3f4c
Inhishonor <inhishonor@protonmail.com>
2024-12-16 11:28:49 -0800

    Update fortune-teller.py

--- fortune-teller.py
+++ fortune-teller.py
@@ -1,11 +1,5 @@
-#Define all Variables/Messages
-ds = "You shall be defenestrated while sitting in a skyscraper!"
-rc = "You will be in a raccon race and suddenly get eaten by a cobra!"
-ud = "You shall see a unicorn and disapear forever!"
-ss = "You will be in the Sahara and get run over by Santa Claus!"
-sc = "You will be walking on a street and suddenly will combust!"
-ni = "You will win Miss. New Mexico and marry an iguana!"
-ys = "You will surive only to die a horrible death! :)"
+#Call's fortune's
+import fortune
 #Ask what the name and age is.
 name = input("What is your first name? ")
 print("Welcome to the Fortune Teller of Doom!!!")
